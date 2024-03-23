6499dea9043da6ce508db13f07c6d98868681d48
yasinbashar <107603927+yasinbashar@users.noreply.github.com>
2024-03-23 17:25:05 +0600

    added frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+// import { Route,Routes,BrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ReportPage from "./pages/Report/ReportPage";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <button class="btn btn-primary">Button</button>
-    </>
+    <div>
+      {/* <BrowserRouter>
+
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/Report" element={<ReportPage/>}/>
+    </Routes>
+
+      </BrowserRouter> */
+      <HomePage/>}
+    </div>
   );
-}
+};
 
 export default App;
