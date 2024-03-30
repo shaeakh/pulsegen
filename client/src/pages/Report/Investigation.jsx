2bff9e148a4825b64dd1f9488de5a34c0b688034
Md. Shaeakh Ahmed Chowdhury <shaeakh12@gmail.com>
2024-03-30 23:08:45 +0600

    .

--- client/src/pages/Report/Investigation.jsx
+++ client/src/pages/Report/Investigation.jsx
@@ -1,30 +1,110 @@
-import React from 'react'
-import Menu from './Menu'
-import Footer1 from './Footer1'
-import Footer2 from './Footer2'
-export default function Investigation() {
-    return (
-        <div>
-            <div className="w-screen flex flex-row">
-
-                <div className="cc w-1/2 my-5 ml-5 mr-2">
-                    <Menu />
-                </div>
-
-
-                <div className="w-1/2">
-                    <div className="present my-5 ml-2 mr-5 bg-gray-100 border-2 border-primary rounded-3xl  p-4 shadow-md">
-                        <h2 className="text-xl font-semibold mb-2" > Present Illness:</h2>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto tempore ducimus atque quia illum enim vero unde optio nisi, ipsum, vel ipsa voluptatum vitae velit laudantium autem consectetur ea fugit?
-                            Accusamus corporis obcaecati corrupti aperiam molestias cum numquam a, quasi sit saepe dicta nisi nulla culpa nostrum atque consequatur ipsam doloremque alias dolorum dolor quisquam molestiae enim. Eveniet, necessitatibus debitis?
-                            Deleniti, id autem repellat nihil eius assumenda! Excepturi expedita, accusantium doloremque fuga minus id odio ullam vitae vel consectetur odit accusamus saepe recusandae qui dolores a soluta doloribus. Dolores, nobis.
-                            Maxime ratione tenetur tempora quasi numquam aspernatur reprehenderit maiores, quam deleniti distinctio minus quibusdam voluptates sequi? Sequi dolorem officia, ratione vitae quo delectus doloremque praesentium quasi quis eius in nostrum.
-                            Sapiente culpa ullam natus, impedit quia veritatis voluptate nobis. Nostrum vitae libero molestias culpa qui dolores, cum dolorum provident, accusantium omnis enim placeat possimus. Nesciunt ipsum recusandae earum voluptas iusto.</p>
-                    </div>
-                </div>
-            </div>
-            <Footer2/>
-            <Footer1/>
+import React from "react";
+import Menu from "./Menu";
+import Footer1 from "./Footer1";
+import Footer2 from "./Footer2";
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+export default function Investigation(props) {
+  const illness = props.p_ill;
+  const past_history = props.past_history;
+  const downloadPDF = () =>{
+    const input = pdfref.current;
+    html2canvas(input).then((canvas)=>{
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p','mm','a4',true);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = pdf.internal.pageSize.getHeight();
+      const imgWidth = canvas.width;
+      const imgHeight = canvas.height;
+      const ratio = Math.min(pdfWidth/imgWidth , pdfHeight /imgHeight);
+      const imgX = (pdfWidth - imgWidth*ratio) / 2 ;
+      imgY = 30;
+      pdf.addImage(imgData,'PNG',imgX,imgY,imgWidth*ratio,imgHeight*ratio);
+      pdf.save("Report.pdf")
+    })
+}
+  return (
+    <div ref={pdfref} >
+      <div>
+      <div className="overflow-x-auto m-5 mt-20">
+        {/* hospital name and logo  */}
+        <div className="w-full h-25  p-5 flex justify-start items-center bg-base-200 mt-2">
+          <div className="font-bold text-xl">{props.doc_name}</div>
+          <div className="font-bold text-xl ml-5">{props.hos_name}</div>
+          <div className="w-max border-2 ml-5">
+            <img
+              src={props.logo_link}
+              className="object-contain h-25 w-20"
+              alt="Logo"
+            />
+          </div>
+          
         </div>
-    )
+        <table className="table">
+          {/* head */}
+          <thead>
+            
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr>
+              <th className="w-36 ">Name</th>
+              <td>{props.p_name}</td>              
+            </tr>
+            {/* row 2 */}
+            <tr>
+              <th className="w-36 ">Age</th>
+              <td>{props.age}</td>
+              
+            </tr>
+            {/* row 3 */}
+            <tr>
+              <th className="w-36 ">Gender</th>
+              <td>{props.gender}</td>
+              
+            </tr>
+            <tr>
+              <th className="w-36 ">Report SL</th>
+              <td>{props.report_sl}</td>              
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+      <div className="w-screen flex flex-row">
+        <div className="cc w-1/2 my-5 ml-5 mr-2 border-2 border-primary rounded-3xl  p-4 shadow-md">
+          <h2 className="text-xl font-semibold mb-2"> Past History:</h2>
+          <table>
+            {past_history.map((history, index) => {
+              return (
+                <tr>
+                  <th className="flex justify-start">{history.type}</th>
+                  <td className="font-bold">:</td>
+                  <td>{history.details}</td>
+                </tr>
+              );
+              }
+            )
+          }
+          </table>
+        </div>
+        <div className="w-1/2">
+          <div className="present my-5 ml-2 mr-5 bg-gray-100 border-2 border-primary rounded-3xl  p-4 shadow-md">
+            <h2 className="text-xl font-semibold mb-2"> Present Illness:</h2>
+
+            {illness.map((ill, index) => {
+              return <li>{ill.illness}</li>;
+            })}
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <div onClick={downloadPDF()} className="btn bg-primary text-white  w-1/6 hover:text-primary hover:font-bold hover:border-primary hover:border-2">
+          Download Report
+        </div>
+      </div>
+      <Footer2 />
+      <Footer1 />
+    </div>
+  );
 }
--- client/src/pages/Specialists/Specialists.jsx
+++ client/src/pages/Specialists/Specialists.jsx
@@ -1,83 +1,238 @@
-
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Doctor from "./Doctor";
 
 export default function Specialists() {
   const Doctors = [
     {
-      depertment: "a", id:"0", name: "a", img: "https://img.freepik.com/free-photo/portrait-expressive-young-man-wearing-formal-suit_273609-6942.jpg", deg: "m",
-      rank: "asd", badge: "asd", tag: "asd", active: true, place: "asd",
-      exp: "4", r_n: "320", rating: "3.2", c_fee: 150, avail_2: "t",
-      avail_t: "t", gender:"male"     
+      depertment: "a",
+      id: "0",
+      name: "a",
+      img: "https://img.freepik.com/free-photo/portrait-expressive-young-man-wearing-formal-suit_273609-6942.jpg",
+      deg: "m",
+      rank: "asd",
+      badge: "asd",
+      tag: "asd",
+      active: true,
+      place: "asd",
+      exp: "4",
+      r_n: "320",
+      rating: "3.2",
+      c_fee: 150,
+      avail_2: "t",
+      avail_t: "t",
+      gender: "male",
     },
     {
-      depertment: "a", id:"1", name: "b", img: "https://img.freepik.com/free-photo/portrait-expressive-young-man-wearing-formal-suit_273609-6942.jpg", deg: "asd",
-      rank: "asd", badge: "asd", tag: "asd", active: true, place: "asd",
-      exp: "9", r_n: "150", rating: "4.1", c_fee: 220, avail_2: "t",
-      avail_t: "f", gender:"male"      
+      depertment: "a",
+      id: "1",
+      name: "b",
+      img: "https://img.freepik.com/free-photo/portrait-expressive-young-man-wearing-formal-suit_273609-6942.jpg",
+      deg: "asd",
+      rank: "asd",
+      badge: "asd",
+      tag: "asd",
+      active: true,
+      place: "asd",
+      exp: "9",
+      r_n: "150",
+      rating: "4.1",
+      c_fee: 220,
+      avail_2: "t",
+      avail_t: "f",
+      gender: "male",
     },
     {
-      depertment: "a", id:"2", name: "c", img: "https://static.vecteezy.com/system/resources/thumbnails/024/354/297/small_2x/business-woman-isolated-illustration-ai-generative-free-photo.jpg", deg: "asd",
-      rank: "asd", badge: "asd", tag: "asd", active: true, place: "asd",
-      exp: "7", r_n: "453", rating: "3.49", c_fee: 190, avail_2: "f",
-      avail_t: "t", gender:"female"     
+      depertment: "a",
+      id: "2",
+      name: "c",
+      img: "https://static.vecteezy.com/system/resources/thumbnails/024/354/297/small_2x/business-woman-isolated-illustration-ai-generative-free-photo.jpg",
+      deg: "asd",
+      rank: "asd",
+      badge: "asd",
+      tag: "asd",
+      active: true,
+      place: "asd",
+      exp: "7",
+      r_n: "453",
+      rating: "3.49",
+      c_fee: 190,
+      avail_2: "f",
+      avail_t: "t",
+      gender: "female",
     },
     {
-      depertment: "a", id:"3", name: "d", img: "https://img.freepik.com/free-photo/portrait-expressive-young-man-wearing-formal-suit_273609-6942.jpg", deg: "asd",
-      deg: "asd", badge: "asd", tag: "asdf", active: true, place: "asd",
-      exp: "2", r_n: "199", rating: "2.5", c_fee: 850, avail_2: "f",
-      avail_t: "f", gender:"male"
+      depertment: "a",
+      id: "3",
+      name: "d",
+      img: "https://img.freepik.com/free-photo/portrait-expressive-young-man-wearing-formal-suit_273609-6942.jpg",
+      deg: "asd",
+      deg: "asd",
+      badge: "asd",
+      tag: "asdf",
+      active: true,
+      place: "asd",
+      exp: "2",
+      r_n: "199",
+      rating: "2.5",
+      c_fee: 850,
+      avail_2: "f",
+      avail_t: "f",
+      gender: "male",
     },
     {
-      depertment: "a", id:"4", name: "e", img: "https://img.freepik.com/free-photo/portrait-expressive-young-man-wearing-formal-suit_273609-6942.jpg", deg: "asd",
-      deg: "asd", badge: "asd", tag: "asdf", active: false, place: "asd",
-      exp: "9", r_n: "1999", rating: "4.9", c_fee: 250, avail_2: "t",
-      avail_t: "t", gender:"male" 
+      depertment: "a",
+      id: "4",
+      name: "e",
+      img: "https://img.freepik.com/free-photo/portrait-expressive-young-man-wearing-formal-suit_273609-6942.jpg",
+      deg: "asd",
+      deg: "asd",
+      badge: "asd",
+      tag: "asdf",
+      active: false,
+      place: "asd",
+      exp: "9",
+      r_n: "1999",
+      rating: "4.9",
+      c_fee: 250,
+      avail_2: "t",
+      avail_t: "t",
+      gender: "male",
     },
     {
-      depertment: "a", id:"5", name: "f", img: "https://static.vecteezy.com/system/resources/thumbnails/024/354/297/small_2x/business-woman-isolated-illustration-ai-generative-free-photo.jpg", deg: "asd",
-      deg: "asd", badge: "asd", tag: "asdf", active: false, place: "asd",
-      exp: "15", r_n: "250", rating: "5", c_fee: 1000, avail_2: "t",
-      avail_t: "f", gender:"female"
+      depertment: "a",
+      id: "5",
+      name: "f",
+      img: "https://static.vecteezy.com/system/resources/thumbnails/024/354/297/small_2x/business-woman-isolated-illustration-ai-generative-free-photo.jpg",
+      deg: "asd",
+      deg: "asd",
+      badge: "asd",
+      tag: "asdf",
+      active: false,
+      place: "asd",
+      exp: "15",
+      r_n: "250",
+      rating: "5",
+      c_fee: 1000,
+      avail_2: "t",
+      avail_t: "f",
+      gender: "female",
     },
     {
-      depertment: "a", id:"6", name: "g", img: "https://img.freepik.com/free-photo/portrait-expressive-young-man-wearing-formal-suit_273609-6942.jpg", deg: "asd",
-      deg: "asd", badge: "asd", tag: "asdf", active: false, place: "asd",
-      exp: "5", r_n: "50", rating: "5", c_fee: 250, avail_2: "f",
-      avail_t: "t", gender:"male"
+      depertment: "a",
+      id: "6",
+      name: "g",
+      img: "https://img.freepik.com/free-photo/portrait-expressive-young-man-wearing-formal-suit_273609-6942.jpg",
+      deg: "asd",
+      deg: "asd",
+      badge: "asd",
+      tag: "asdf",
+      active: false,
+      place: "asd",
+      exp: "5",
+      r_n: "50",
+      rating: "5",
+      c_fee: 250,
+      avail_2: "f",
+      avail_t: "t",
+      gender: "male",
     },
     {
-      depertment: "a", id:"7", name: "h", img: "https://static.vecteezy.com/system/resources/thumbnails/024/354/297/small_2x/business-woman-isolated-illustration-ai-generative-free-photo.jpg", deg: "asd",
-      deg: "asd", badge: "asd", tag: "asdf", active: false, place: "asd",
-      exp: "9", r_n: "500", rating: "4.6", c_fee: 500, avail_2: "f",
-      avail_t: "f", gender:"female"
+      depertment: "a",
+      id: "7",
+      name: "h",
+      img: "https://static.vecteezy.com/system/resources/thumbnails/024/354/297/small_2x/business-woman-isolated-illustration-ai-generative-free-photo.jpg",
+      deg: "asd",
+      deg: "asd",
+      badge: "asd",
+      tag: "asdf",
+      active: false,
+      place: "asd",
+      exp: "9",
+      r_n: "500",
+      rating: "4.6",
+      c_fee: 500,
+      avail_2: "f",
+      avail_t: "f",
+      gender: "female",
     },
     {
-      depertment: "a", id:"8", name: "i", img: "https://static.vecteezy.com/system/resources/thumbnails/024/354/297/small_2x/business-woman-isolated-illustration-ai-generative-free-photo.jpg", deg: "asd",
-      deg: "asd", badge: "asd", tag: "asdf", active: true, place: "asd",
-      exp: "2", r_n: "166", rating: "3.6", c_fee: 145, avail_2: "f",
-      avail_t: "t", gender:"female"
+      depertment: "a",
+      id: "8",
+      name: "i",
+      img: "https://static.vecteezy.com/system/resources/thumbnails/024/354/297/small_2x/business-woman-isolated-illustration-ai-generative-free-photo.jpg",
+      deg: "asd",
+      deg: "asd",
+      badge: "asd",
+      tag: "asdf",
+      active: true,
+      place: "asd",
+      exp: "2",
+      r_n: "166",
+      rating: "3.6",
+      c_fee: 145,
+      avail_2: "f",
+      avail_t: "t",
+      gender: "female",
     },
     {
-      depertment: "a", id:"9", name: "j", img: "https://static.vecteezy.com/system/resources/thumbnails/024/354/297/small_2x/business-woman-isolated-illustration-ai-generative-free-photo.jpg", deg: "asd",
-      deg: "asd", badge: "asd", tag: "asdf", active: true, place: "asd",
-      exp: "5", r_n: "197", rating: "3.9", c_fee: 200, avail_2: "f",
-      avail_t: "t", gender:"female"
+      depertment: "a",
+      id: "9",
+      name: "j",
+      img: "https://static.vecteezy.com/system/resources/thumbnails/024/354/297/small_2x/business-woman-isolated-illustration-ai-generative-free-photo.jpg",
+      deg: "asd",
+      deg: "asd",
+      badge: "asd",
+      tag: "asdf",
+      active: true,
+      place: "asd",
+      exp: "5",
+      r_n: "197",
+      rating: "3.9",
+      c_fee: 200,
+      avail_2: "f",
+      avail_t: "t",
+      gender: "female",
     },
     {
-      depertment: "a", id:"10", name: "k", img: "https://static.vecteezy.com/system/resources/thumbnails/024/354/297/small_2x/business-woman-isolated-illustration-ai-generative-free-photo.jpg", deg: "asd",
-      deg: "asd", badge: "asd", tag: "asdf", active: false, place: "asd",
-      exp: "5.6", r_n: "102", rating: "2.3", c_fee: 450, avail_2: "t",
-      avail_t: "t", gender:"female"
+      depertment: "a",
+      id: "10",
+      name: "k",
+      img: "https://static.vecteezy.com/system/resources/thumbnails/024/354/297/small_2x/business-woman-isolated-illustration-ai-generative-free-photo.jpg",
+      deg: "asd",
+      deg: "asd",
+      badge: "asd",
+      tag: "asdf",
+      active: false,
+      place: "asd",
+      exp: "5.6",
+      r_n: "102",
+      rating: "2.3",
+      c_fee: 450,
+      avail_2: "t",
+      avail_t: "t",
+      gender: "female",
     },
     {
-      depertment: "a", id:"11", name: "l", img: "https://img.freepik.com/free-photo/portrait-expressive-young-man-wearing-formal-suit_273609-6942.jpg", deg: "asd",
-      deg: "asd", badge: "asd", tag: "asdf", active: false, place: "asd",
-      exp: "10", r_n: "1000", rating: "5", c_fee: 500, avail_2: "f",
-      avail_t: "t", gender:"male"
-    }  
+      depertment: "a",
+      id: "11",
+      name: "l",
+      img: "https://img.freepik.com/free-photo/portrait-expressive-young-man-wearing-formal-suit_273609-6942.jpg",
+      deg: "asd",
+      deg: "asd",
+      badge: "asd",
+      tag: "asdf",
+      active: false,
+      place: "asd",
+      exp: "10",
+      r_n: "1000",
+      rating: "5",
+      c_fee: 500,
+      avail_2: "f",
+      avail_t: "t",
+      gender: "male",
+    },
   ];
   const [cfee_min, setCfee_min] = useState(0);
   const [cfee_max, setCfee_max] = useState(999999999999);
@@ -88,7 +243,7 @@ export default function Specialists() {
     availableToday: false,
     maleOnly: false,
     femaleOnly: false,
-    sortBy: ''
+    sortBy: "",
   });
 
   const handleCheckboxChange = (event) => {
@@ -104,35 +259,35 @@ export default function Specialists() {
 
   let filteredDoctors = [...Doctors];
   if (filters.onlineNow) {
-    filteredDoctors = filteredDoctors.filter(doc => doc.active);
+    filteredDoctors = filteredDoctors.filter((doc) => doc.active);
   }
   if (filters.availableNext2Hours) {
-    filteredDoctors = filteredDoctors.filter(doc => doc.avail_2 === "t");
+    filteredDoctors = filteredDoctors.filter((doc) => doc.avail_2 === "t");
   }
   if (filters.availableToday) {
-    filteredDoctors = filteredDoctors.filter(doc => doc.avail_t === "t");
+    filteredDoctors = filteredDoctors.filter((doc) => doc.avail_t === "t");
   }
   if (filters.maleOnly) {
-    filteredDoctors = filteredDoctors.filter(doc => doc.gender === "male");
+    filteredDoctors = filteredDoctors.filter((doc) => doc.gender === "male");
   }
   if (filters.femaleOnly) {
-    filteredDoctors = filteredDoctors.filter(doc => doc.gender === "female");
+    filteredDoctors = filteredDoctors.filter((doc) => doc.gender === "female");
   }
   if (filters.sortBy) {
     switch (filters.sortBy) {
-      case 'feesHighToLow':
+      case "feesHighToLow":
         filteredDoctors.sort((a, b) => b.c_fee - a.c_fee);
         break;
-      case 'feesLowToHigh':
+      case "feesLowToHigh":
         filteredDoctors.sort((a, b) => a.c_fee - b.c_fee);
         break;
-      case 'rating':
+      case "rating":
         filteredDoctors.sort((a, b) => b.rating - a.rating);
         break;
-      case 'experience':
+      case "experience":
         filteredDoctors.sort((a, b) => b.exp - a.exp);
         break;
-      case 'popularity':
+      case "popularity":
         filteredDoctors.sort((a, b) => b.r_n - a.r_n);
         break;
       default:
@@ -140,9 +295,6 @@ export default function Specialists() {
     }
   }
 
-  
-
-  
   // console.log(cfee_min);
   // console.log(cfee_max);
   // let cfee_min=0;
@@ -181,14 +333,13 @@ export default function Specialists() {
             </div>
             <div className="flex flex-col w-full my-5 pl-5">
               <div className="flex flex-row justify-start ml-5 ">
-                  <input
-                    type="checkbox"
-                    name="OnlineNow"
-                    onChange={handleCheckboxChange}
-                    checked={filters.onlineNow}
-                    
-                    className="checkbox checkbox-sm mr-2"
-                  />
+                <input
+                  type="checkbox"
+                  name="OnlineNow"
+                  onChange={handleCheckboxChange}
+                  checked={filters.onlineNow}
+                  className="checkbox checkbox-sm mr-2"
+                />
                 <h1>Online Now</h1>
               </div>
               <div className="flex flex-row justify-start ml-5 mt-2">
@@ -224,7 +375,6 @@ export default function Specialists() {
               <div className="flex flex-row justify-start ml-5 mt-2">
                 <input
                   type="checkbox"
-                  
                   onChange={handleCheckboxChange}
                   name="maleOnly"
                   defaultChecked
@@ -347,29 +497,29 @@ export default function Specialists() {
           </div>
 
           <div className="w-4/5 p-5 ">
-            {Doctors.map((D,i)=>{              
-              return <Doctor
-                depertment={D.depertment} 
-                id = {D.id}
-                name = {D.name}
-                img = {D.img}
-                deg = {D.deg}
-                rank = {D.rank}
-                badge = {D.badge}
-                tag = {D.tag}
-                active = {D.active}
-                place = {D.place}
-                exp = {D.exp}
-                r_n = {D.r_n}
-                rating = {D.rating}
-                c_fee = {D.c_fee}
-                avail_2 = {D.avail_2}
-                avail_t = {D.avail_t}
-                gender = {D.gender}
+            {Doctors.map((D, i) => {
+              return (
+                <Doctor
+                  depertment={D.depertment}
+                  id={D.id}
+                  name={D.name}
+                  img={D.img}
+                  deg={D.deg}
+                  rank={D.rank}
+                  badge={D.badge}
+                  tag={D.tag}
+                  active={D.active}
+                  place={D.place}
+                  exp={D.exp}
+                  r_n={D.r_n}
+                  rating={D.rating}
+                  c_fee={D.c_fee}
+                  avail_2={D.avail_2}
+                  avail_t={D.avail_t}
+                  gender={D.gender}
                 ></Doctor>
-              })          
-         }
-            
+              );
+            })}
           </div>
         </div>
       </main>
