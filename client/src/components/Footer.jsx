225a27078f6d4748f051b5ed5ee8cf57cb40001c
yasinbashar <107603927+yasinbashar@users.noreply.github.com>
2024-04-06 21:28:59 +0600

    footer changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
     <div className=" bg-primary text-white rounded-t-3xl mt-8 md:mt-0">
       <div className="flex flex-col md:flex-row justify-between p-8 md:px-32 px-5">
         <div className=" w-full md:w-1/4">
-          <h1 className=" font-semibold text-xl pb-4">WellnessVista</h1>
+          <h1 className=" font-semibold text-xl pb-4">Pulsegen</h1>
           <p className=" text-sm">
             Our team of dedicated doctors, each specializing in unique fields
-            such as orthopedics, cardiology, pediatrics, neurology, dermatology,
+            such as psycology, cardiology, gynae, neurology
             and more.
           </p>
         </div>
@@ -55,7 +55,7 @@ const Footer = () => {
               duration={500}
               className=" hover:text-hoverColor transition-all cursor-pointer"
             >
-              Lab Test
+              AI Test
             </Link>
             <Link
               to="services"
@@ -64,7 +64,7 @@ const Footer = () => {
               duration={500}
               className=" hover:text-hoverColor transition-all cursor-pointer"
             >
-              Health Check
+              Instant primary diagnostic report
             </Link>
             <Link
               to="services"
@@ -73,7 +73,7 @@ const Footer = () => {
               duration={500}
               className=" hover:text-hoverColor transition-all cursor-pointer"
             >
-              Heart Health
+             Doctor's consultation
             </Link>
           </nav>
         </div>
@@ -81,24 +81,24 @@ const Footer = () => {
           <h1 className=" font-medium text-xl pb-4 pt-5 md:pt-0">Contact Us</h1>
           <nav className=" flex flex-col gap-2">
             <Link to="/" spy={true} smooth={true} duration={500}>
-              123 Elm Street, Suite 456 Springfield, IL 62701 United States
+              Kumargaon, Sylhet 3114
             </Link>
             <Link to="/" spy={true} smooth={true} duration={500}>
-              support@care.com
+              shaeakh12@gmail.com
             </Link>
             <Link to="/" spy={true} smooth={true} duration={500}>
-              +123-456-7890
+             01704567731
             </Link>
           </nav>
         </div>
       </div>
-      <div>
+      {/* <div>
         <p className=" text-center py-4">
           @copyright developed by
           <span className=" text-hoverColor"> champion programmers</span> | All
           rights reserved
         </p>
-      </div>
+      </div> */}
     </div>
   );
 };
